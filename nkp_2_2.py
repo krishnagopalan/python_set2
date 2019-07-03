0cc6478ceb73f58d46e353fa35af54ccd80dea8f
krishnagopalan <51943914+krishnagopalan@users.noreply.github.com>
2019-07-03 17:12:03 +0530

    Update nkp_2_2.py

--- nkp_2_2.py
+++ nkp_2_2.py
@@ -6,6 +6,6 @@ while(n>0):
     rev=rev*10+dig
     n=n//10
 if(temp==rev):
-    print("The number is a palindrome!")
+    print("yes")
 else:
-    print("The number isn't a palindrome!")
+    print("no")
